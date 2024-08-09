849209902d67efb7006960200ea7c93bc26b1750
hosseinGith <hosseinderee7@gmail.com>
2024-08-09 14:11:15 +0430

    added all files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      mainBgColor: "#000",
+      mainYellow: "#ffe971",
+      whiteColor: "#fff",
+      blackColor: "#000",
+    },
+  },
+  plugins: [],
+};
